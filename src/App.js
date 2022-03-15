b8319bbb81059341a6517bff579d8ad8b45fb3ad
brewchetta <chett.tiller@gmail.com>
2022-03-15 12:04:26 -0400

    burger rater!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import BurgerList from './BurgerList'
+import burgers from './burger-data'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Hamburger Rater</h1>
+
+      <img src="https://emojipedia-us.s3.amazonaws.com/social/emoji/hamburger.png" alt="image of all the burgers!" />
+
+      <BurgerList burgers={burgers} catchphrase="don't eat pandas" />
+
+      {/* { burgers: Array, catchphrase: "don't eat pandas" }*/}
+
     </div>
   );
 }
